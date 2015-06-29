450f38f7dabe21769d0b5afbc1fe4d626678eaa9
ecoronado89 <coreddin@gmail.com>
2015-06-29 09:51:30 -0600

    minor changes

--- ProdPage/src/defaultPackage/TestingProductPage.java
+++ ProdPage/src/defaultPackage/TestingProductPage.java
@@ -2,10 +2,7 @@ package defaultPackage;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import jxl.Sheet;
 import jxl.read.biff.BiffException;
@@ -24,48 +21,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.jayway.restassured.specification.RequestSpecification;
 
 
 @SuppressWarnings("deprecation")
 public class TestingProductPage {
 	
-	WebDriver driver;
+	static WebDriver driver;
 	EventFiringWebDriver eventFiringWebDriver;
 	String mainWindowHandle;
 	String page;
-	RequestSpecification requestSpecification;
 
 
 	@BeforeTest
 	public void testSetUp(){
-		// create a driver
-			
-//		Internet Explorer		
-//		File file = new File("C:/litofiles/Automation/Research/Selenium/IEDriverServer.exe");		
-//		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
-//		DesiredCapabilities dc = DesiredCapabilities.internetExplorer();
-//		dc.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);  //If IE fail to work, please remove this and remove enable protected mode for all the 4 zones from Internet options
-//		driver = new InternetExplorerDriver(dc);
-
-//		Firefox		
-//	     driver = new FirefoxDriver();
-
-
-//		Chrome
+		
+//		Chrome Driver
 		File file = new File("C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		driver = new ChromeDriver(DesiredCapabilities.chrome());
 		
 		
 		eventFiringWebDriver = new EventFiringWebDriver (driver);
-
-//	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().deleteAllCookies();
         
        
@@ -75,32 +58,40 @@ public class TestingProductPage {
 	
 	@AfterTest
 	public void testShutDown(){
-		//ShutDown selenium driver
+		
 		driver.close();
 	}
 	
 	@Test
 	public void test() throws BiffException, IOException{
 		
-		//String[] pages = {"74891","444444","31179", "83440"};
-	//	Sheet sheet = HandleInput.readFile();
-		//if (sheet != null){
-		//	int rows = sheet.getRows();
-		//	for (int i = 0; i < rows; i++) {
-	      //  	page = sheet.getCell(0,i).getContents();
-	        	driver.get("http://ecwebs01.llbean.com/llb/shop/"+36069);
+		
+		Sheet sheet = HandleInput.readFile();
+		if (sheet != null){
+			int rows = sheet.getRows();
+			for (int i = 0; i < rows; i++) {
+				page = sheet.getCell(0,i).getContents();
+	        	driver.get("http://www.llbean.com/llb/shop/"+page);
 	            mainWindowHandle = driver.getWindowHandles().iterator().next();
-	            //valAllSwatches();
 	            
-	            if (isPageAvailable()&&isProductAvailable()&&validateSizeChart()&&verifyImage()){
-	            	//isSoldOut();
-	            	System.out.println("Processing page: "+page);
+	            
+	            System.out.println("\n********* Processing page: "+page+ " *********");
+	            	
+	            if(isPageAvailable() == true)
+	            {
+	            	isSoldOut();
+		            isProductAvailable();
+		            validateSizeChart();
+		            validateBreadcrum();
+	            	verifyImage();
+	            	
 	            }
-			//}
+	        
+			}
 			
-	//	}else{
-		//	System.out.println("Sheet of pages is null.");
-	//	}
+		}else{
+			System.out.println("Sheet of pages is null.");
+		}
 	}
 	
 	private boolean isPageAvailable(){
@@ -110,7 +101,7 @@ public class TestingProductPage {
 		//looks for Page Not Available image
 		if (driver.getTitle().equalsIgnoreCase(notAvailTitle)){
 			available = false;
-			System.out.println("*********Page: "+page+" Not Available");
+			System.err.println("Page is Not Available");
 		}    
 		return available;
 	}
@@ -138,11 +129,11 @@ public class TestingProductPage {
 		Boolean prodAvailable = true;
 		try{
 			driver.findElement(By.cssSelector(Selector.PROD_AVAIL));
-			System.out.println("*********Product at page: "+ page + " is NOT available;");			
+			System.err.println("Product is NOT available;");			
 	        prodAvailable = false;
 	        
 		}catch (NoSuchElementException n){
-			//System.out.println("Product at page: "+ page + " is available;");
+			
 		}
 		return prodAvailable;
 	}
@@ -165,12 +156,12 @@ public class TestingProductPage {
 			WebElement priceCont = driver.findElement(By.cssSelector(Selector.ITM_PRICE));
 			WebElement redPrice = priceCont.findElement(By.cssSelector(Selector.SOLD_OUT));
 			if(redPrice.getText().equalsIgnoreCase("Sold Out")){
-				System.out.println("Page: "+ page + " is Sold Out");
+				System.err.println("Page is Sold Out");
 				soldOut = false;	
 			}		
 	        
 		}catch (NoSuchElementException n){
-			//System.out.println("Product at page: "+ page + " is available;");
+			
 		}
 		return soldOut;
 	}
@@ -257,7 +248,8 @@ public class TestingProductPage {
 		}else{	
 			System.out.println("No item price container present");
 		}
-	}
+	}//*[@id="breadcrumbs"]
+	
 	
   private Boolean isBrokenImageRepsonse(String imgUrl) {
 	    Boolean isBroken = false;
@@ -290,29 +282,39 @@ public class TestingProductPage {
 			driver.findElement(By.cssSelector(Selector.SCHART));
 			sizeChart = true;
 		} catch(NoSuchElementException n){
-			System.err.println("Page: "+ page + " without Size Chart");
+			System.err.println("Size Chart Not available");
 		}
 		return sizeChart;
 		
 		}
 	
-	
-	private boolean verifyImage() 
+	//Validate if Hero image is displayed
+	private static boolean verifyImage() 
 	{
 		Boolean HImage = false;
-		
-		String heroImage = driver.findElement(By.xpath("id('backImageSjElement4_img')")).getAttribute("src");
-		
-		if(heroImage.contains("img_not_avail"))
-		{
-			System.err.println("Hero image is broken");
-			validateAV();
+		try{
+			String heroImage = driver.findElement(By.xpath("//*[@id='backImageSjElement4_img']")).getAttribute("src");
+			
+			try{
+				if(heroImage.contains("img_not_avail"))
+				{
+					System.err.println("Hero image is broken");
+					HImage = true;
+				}
+							
+				validateAV();
+			}catch(NullPointerException e){
+				System.err.print("Source not found");
+			}
+		} catch(NoSuchElementException n){
+			System.err.println("Image Not loaded");
 		}
-		else
-			System.out.println("Hero image displayed");
-			return HImage;
+		return HImage;
 	}
-	public void validateAV(){
+	
+	//Validate if all AV are correct
+	public static void validateAV(){
+		try{
 		List<WebElement> AVimages = driver.findElements(By.xpath("//*[@id='ppAlternateViews']/div"));
 				
 	
@@ -322,12 +324,28 @@ public class TestingProductPage {
 			 String AV = driver.findElement(By.xpath("//*[@id='ppAlternateViews']/div["+alt+"]/a/img")).getAttribute("src");
 			 if(AV.contains("IMG_not_avail_"))
 			 	 System.err.println("Alternate View "+alt+" is not available");
-			 else 
-				 System.out.println("Alternate View "+alt+" displayed");
+			
 			 
 		 }
+		}catch(NullPointerException e){
+		System.err.print("Product does not contain Alternate views");
+		}
 		
 	}
+	
+	private boolean validateBreadcrum(){
+		Boolean breadC = false;
+		try{
+			driver.findElement(By.cssSelector(Selector.BREADC));
+			breadC = true;
+		} catch(NoSuchElementException n){
+			System.err.println("Breadcrum Not available");
+		}
+		return breadC;
+		
+		}
+	
+	
 	
 	}
 
